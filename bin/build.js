f9d8b9ce12aa26f3a2931547654b5faf474f0045
Vaaaaal <45296045+Vaaaaal@users.noreply.github.com>
2025-12-19 11:28:17 +0100

    Revert "add intro animation home"

--- bin/build.js
+++ bin/build.js
@@ -7,7 +7,7 @@ const BUILD_DIRECTORY = 'dist';
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 // Config entrypoint files
-const ENTRY_POINTS = ['src/index.js', 'src/home.js', 'src/services.js'];
+const ENTRY_POINTS = ['src/index.js', 'src/services.js'];
 
 // Config dev serving
 const LIVE_RELOAD = !PRODUCTION;
