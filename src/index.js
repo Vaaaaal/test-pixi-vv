ab59d559c85ddc79956ccc77a8929f180e5d844d
Vaaaaal <cassusval@gmail.com>
2026-04-13 18:55:31 +0200

    Change JS for navbar element with background

--- src/index.js
+++ src/index.js
@@ -4,6 +4,43 @@ window.Webflow ||= [];
 window.Webflow.push(() => {
   processRichtextResponsiveImages();
 
+  // Animation de la navbar au scroll (uniquement si .nav_background est présent)
+  const navComponent = document.querySelector('.nav_component');
+  const navBackground = navComponent ? navComponent.querySelector('.nav_background') : null;
+  if (navBackground) {
+    const threshold = window.innerHeight * 0.05; // 5vh
+    let isActive = false;
+    const navParent = navComponent.parentElement;
+    const isDark = navParent.classList.contains('u-theme-dark');
+
+    const activate = () => {
+      isActive = true;
+      gsap.to(navBackground, { opacity: 1, duration: 0.3, ease: 'power2.out' });
+
+      if (isDark) {
+        navParent.classList.remove('u-theme-dark');
+      }
+    };
+
+    const deactivate = () => {
+      isActive = false;
+      gsap.to(navBackground, { opacity: 0, duration: 0.3, ease: 'power2.out' });
+
+      if (isDark) {
+        navParent.classList.add('u-theme-dark');
+      }
+    };
+
+    window.addEventListener(
+      'scroll',
+      () => {
+        if (window.scrollY >= threshold && !isActive) activate();
+        else if (window.scrollY < threshold && isActive) deactivate();
+      },
+      { passive: true }
+    );
+  }
+
   document.querySelectorAll('.w-richtext').forEach((el) => {
     el.innerHTML = el.innerHTML.replace(/ ([;:!?»])/g, '\u00A0$1').replace(/([«]) /g, '$1\u00A0');
   });
